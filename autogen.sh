9960e7cc37e7f86f655cdc01d8b5e1e366463c0c
giuppe3d <>
2007-06-13 17:49:08 +0000

    patch to obtain correct RESOURCE_PATH

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 rm -f config.cache
 autoheader
-#aclocal 
-autoconf
+aclocal
+echo "AC_DEFUN(EPIPHANY_EXPAND_DIR, [" >>aclocal.m4
+echo "\$1=\$2" >>aclocal.m4
+echo "\$1=\`(" >>aclocal.m4
+echo "test \"x\$prefix\" = xNONE && prefix=\"\$ac_default_prefix\"" >>aclocal.m4
+echo "test \"x\$exec_prefix\" = xNONE && exec_prefix=\"\${prefix}\"" >>aclocal.m4
+echo "eval echo \\\"\"[\$]\$1\"\\\"" >>aclocal.m4
+echo ")\`" >>aclocal.m4
+echo "])" >>aclocal.m4
+ autoconf
 automake -a --add-missing
 exit
