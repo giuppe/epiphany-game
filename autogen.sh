4ce8de6053c33508a61397c3cf9eb4c8f3209171
giuppe3d <>
2007-06-14 11:41:24 +0000

    fixex in autotools

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 rm -f config.cache
+aclocal -I aclocal
 autoheader
-aclocal
-echo "AC_DEFUN(EPIPHANY_EXPAND_DIR, [" >>aclocal.m4
-echo "\$1=\$2" >>aclocal.m4
-echo "\$1=\`(" >>aclocal.m4
-echo "test \"x\$prefix\" = xNONE && prefix=\"\$ac_default_prefix\"" >>aclocal.m4
-echo "test \"x\$exec_prefix\" = xNONE && exec_prefix=\"\${prefix}\"" >>aclocal.m4
-echo "eval echo \\\"\"[\$]\$1\"\\\"" >>aclocal.m4
-echo ")\`" >>aclocal.m4
-echo "])" >>aclocal.m4
- autoconf
+autoconf
 automake -a --add-missing
 exit
