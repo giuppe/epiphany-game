7635d9799d1d851e594591cfc3cf28608ff21635
giuppe3d <>
2007-07-10 18:14:34 +0000

    inserted license disclaimer

--- src/fonts/font.cpp
+++ src/fonts/font.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+                          font.cpp  -  description
+                             -------------------
+    begin                : Jun 13 2007
+    copyright            : (C) 2007 by Giuseppe D'Aqui'
+    email                : kumber@tiscalinet.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License, Version 2,      *
+ *   as published by the Free Software Foundation.                         *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "font.h"
 #include "SFont.h"
 #include "screen.h"
