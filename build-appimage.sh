dac921a927472d6487fee6781750e04813f782e6
giuppe <giuseppe.da@gmail.com>
2026-07-10 23:47:23 +0200

    added linuxdeploy source link

--- build-appimage.sh
+++ build-appimage.sh
@@ -2,5 +2,7 @@
 make distclean
 ./configure --prefix=/usr
 make DESTDIR=$PWD/AppDir install
+# download linuxdeploy from github
+# https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 ./linuxdeploy-x86_64.AppImage  --appdir AppDir --desktop-file appdir-src/epiphany.desktop --icon-file appdir-src/epiphany.png --output appimage
 mv Epiphany-x86_64.AppImage ../Epiphany-0.9.0-x86_64.AppImage 
