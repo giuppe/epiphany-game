e0b9aeaaea636a842cb0fab30bb593c8af285794
giuppe <giuseppe.da@gmail.com>
2026-07-11 18:01:12 +0200

    Version 0.9.1

--- build-appimage.sh
+++ build-appimage.sh
@@ -5,4 +5,4 @@ make DESTDIR=$PWD/AppDir install
 # download linuxdeploy from github
 # https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 ./linuxdeploy-x86_64.AppImage  --appdir AppDir --desktop-file appdir-src/epiphany.desktop --icon-file appdir-src/epiphany.png --output appimage
-mv Epiphany-x86_64.AppImage ../Epiphany-0.9.0-x86_64.AppImage 
+mv Epiphany-x86_64.AppImage ../Epiphany-0.9.1-x86_64.AppImage 
